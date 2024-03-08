b3a8f35e1af6f96419191ba5dc05ab1c10ab147f
Armin Darvish <armindarvish@gmail.com>
2024-03-07 17:17:49 -0800

    make binding key in emabrk-file-map optional

--- extras/consult-mu-compose-embark.el
+++ extras/consult-mu-compose-embark.el
@@ -23,21 +23,19 @@
   "Run `consult-mu-attach-files' on the candidate."
     (funcall (apply-partially #'consult-mu-compose-attach cand)))
 
-;;; add attach key to embark-file-map
-(defun consult-mu-compose-embark-bind-attach-file-key (&toptional key)
-  "Binds KEY to `consult-mu-embark-attach-file-key' in `embark-file-map'.
-If KEY is non-nil, uses KEY instead of `consult-mu-embark-attach-file-key'.
-"
-  (if-let ((key (or key (kbd consult-mu-embark-attach-file-key))))
-      (define-key embark-file-map key #'consult-mu-compose-embark-attach-file)))
+;;; add consult-mu-attach to embark-file-map
+(defun consult-mu-compose-embark-bind-attach-file-key (&optional key)
+"Binds `consult-mu-embark-attach-file-key' to `consult-mu-compose-embark-attach-file' in `embark-file-map'.
+
+If KEY is non-nil binds KEY instead of `consult-mu-embark-attach-file-key'."
+(if-let ((keyb (or key (kbd consult-mu-embark-attach-file-key))))
+(define-key embark-file-map keyb #'consult-mu-compose-embark-attach-file)))
 
 (consult-mu-compose-embark-bind-attach-file-key)
 
 ;; change the default action on `consult-mu-contacts category.
 (add-to-list 'embark-default-action-overrides '((file . consult-mu-compose--read-file-attach)  . consult-mu-compose-attach))
 (add-to-list 'embark-default-action-overrides '((file . consult-mu-compose-attach)  . consult-mu-compose-attach))
-
-
 
 ;;; Provide `consult-mu-compose-embark' module
 
